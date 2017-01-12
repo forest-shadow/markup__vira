e098fd7317dd3bf82a325a6275a1ce1888694b40
forest-shadow <mamasay@yandex.ru>
2017-01-12 16:30:14 +0300

    home page markup

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -1,3 +1,59 @@
 $( document ).ready( function(){
+    // initialize navigation
+    $( "#top-nav" ).menumaker( {
+        title: "Меню",
+        format: "multitoggle"
+    } );
 
+    // initialize mainpage slider
+    $( '.js-mainpage-slider' ).slick( {
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000
+    } );
+
+    // initialize our services carousel
+    $( '.js-our-services-carousel' ).slick( {
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000
+    } );
+
+    // initialize our partners
+    $( '.js-our-partners-carousel' ).slick( {
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000
+    } );
+
+    // initialize thanks letters gallery
+    $( '.js-thanks-letters-gallery' ).magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
 });
