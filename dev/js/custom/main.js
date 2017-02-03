3a2542032b576cc76bd1210d8916db259f883cfc
forest-shadow <mamasay@yandex.ru>
2017-02-04 02:53:51 +0300

    add services page + modal

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -78,4 +78,36 @@ $( document ).ready( function(){
       }
     }
   });
+
+
+  // 04. Feedback call popup
+  $( '.js-popup-form' ).magnificPopup({
+    type: 'inline',
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    },
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+
 });
